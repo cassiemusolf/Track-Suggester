aa0d6217009a0914580337380e45d9f913501b91
Cassie Musolf <cassiemusolf@gmail.com>
2017-01-13 10:51:32 -0800

    add value to form options in order to test javascript with html file, if then statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
 $(document).ready(function() {
-  $("form#survey").submit(functon(event) {
+  $("form#survey").submit(function() {
     event.preventDefault();
-    var first = $("#").val());
-    var second = $("#").val());
-    var third = $("#").val());
-    var fourth = $("#").val());
-    var fifth = $("#").val());
+    var first = $("#first").val();
+    var second = $("#second").val();
+    var third = $("#third").val();
+    var fourth = $("#fourth").val();
+    var fifth = $("#fifth").val();
 
-      if () {
-        $("").show();
-      } else if () {
-        $("").show();
-      } else if () {
-        $("").show() {
-      }
-
+      if (first === "front-end") {
+        $(".css").show();
+      } else if (first === "back-end") {
+       $(".c").show();
+     }
   });
 });
